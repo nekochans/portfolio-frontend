88996e2ec479eb142e1dbb4325ee785ebc5140dc
keitakn <keita.koga.work@gmail.com>
2019-11-24 23:18:35 +0900

    :recycle: #45 Storybookの書き方をComponent Story Format(CSF)を使った書き方に変更

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,15 +2,9 @@ import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
-import requireContext from 'require-context.macro';
 
-const req = requireContext('../src/components', true, /.(story|stories).tsx$/);
+addDecorator(withInfo);
+addDecorator(withKnobs);
+addDecorator(withA11y);
 
-function loadStories() {
-  addDecorator(withInfo);
-  addDecorator(withKnobs);
-  addDecorator(withA11y);
-  req.keys().forEach(req);
-}
-
-configure(loadStories, module);
+configure(require.context('../src/components', true, /\.stories\.tsx$/), module);
--- src/components/Footer.stories.tsx
+++ src/components/Footer.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import Footer from './Footer';
 
-storiesOf('Footer', module).add('show Footer', () => {
-  return <Footer />;
-});
+export default {
+  title: 'Footer',
+};
+
+export const showFooter = () => <Footer />;
