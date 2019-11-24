d54d94173ac4f91d38fbfa271ffc7217f68df47a
keitakn <keita.koga.work@gmail.com>
2019-11-24 22:23:04 +0900

    :wrench: #45 @storybook/addon-a11yが読み込まれていなかったので修正

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 import requireContext from 'require-context.macro';
 
 const req = requireContext('../src/components', true, /.(story|stories).tsx$/);
@@ -8,6 +9,7 @@ const req = requireContext('../src/components', true, /.(story|stories).tsx$/);
 function loadStories() {
   addDecorator(withInfo);
   addDecorator(withKnobs);
+  addDecorator(withA11y);
   req.keys().forEach(req);
 }
 
