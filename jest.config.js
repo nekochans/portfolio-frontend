8aa38238b170d60ef7371ca93ca4f10e0bea88b4
keitakn <keita.koga.work@gmail.com>
2021-10-13 16:42:25 +0900

    :wrench: #114 スナップショットテストの方針が決まるまでは不要な設定なので削除

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,6 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '\\.(scss)$': 'identity-obj-proxy',
-  },
   // https://github.com/zeit/next.js/issues/8663#issue-490553899
   globals: {
     // we must specify a custom tsconfig for tests because we need the typescript transform
