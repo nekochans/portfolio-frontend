d89637c1a2b2358ffdc4af950ff14f05f1659c77
keitakn <keita.koga.work@gmail.com>
2021-10-14 12:13:21 +0900

    :recycle: #114 フッター部分はComponentを利用するように変更

--- src/pages/isr.tsx
+++ src/pages/isr.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
+import Footer from '../components/Footer';
 
 type Props = {
   nowDatetime: string;
@@ -24,18 +24,7 @@ const IsrPage: NextPage<Props> = ({ nowDatetime }: Props) => (
       <p className={styles.description}>現在日時は {nowDatetime}</p>
     </main>
 
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
-    </footer>
+    <Footer />
   </div>
 );
 
--- src/templates/GitHubRepositoryTemplate.tsx
+++ src/templates/GitHubRepositoryTemplate.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import CardList from '../components/CardList';
 import useFetchPublicRepos from '../hooks/useFetchPublicRepos';
+import Footer from '../components/Footer';
 
 const GitHubRepositoryTemplate: React.VFC = () => {
   const publicReposRes = useFetchPublicRepos();
@@ -32,18 +32,7 @@ const GitHubRepositoryTemplate: React.VFC = () => {
         )}
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 };
