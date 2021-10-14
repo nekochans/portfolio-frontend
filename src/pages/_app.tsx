3b3e8842bb27296bee56d1384d954d3d4d7e8a7c
keitakn <keita.koga.work@gmail.com>
2021-10-14 11:50:29 +0900

    :recycle: #114 より具体的なCustomAppという名前に変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
 
-export default MyApp;
+export default CustomApp;
