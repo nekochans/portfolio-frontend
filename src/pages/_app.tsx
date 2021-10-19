15878c52082fe24422b3fa092e17b1ef2778d741
keitakn <keita.koga.work@gmail.com>
2021-10-19 22:08:54 +0900

    :sparkles: #130 GoogleAnalyticsの設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,31 @@
 import '../../styles/globals.css';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/gtag';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
-const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
-);
+const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <GoogleAnalytics />
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default CustomApp;
