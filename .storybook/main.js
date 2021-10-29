4990ce728f9f65e3d14c1a3d6c37d5997093daca
keitakn <keita.koga.work@gmail.com>
2021-10-29 17:32:39 +0900

    :arrow_up: #138 Next.jsを含むpackageを最新安定版に更新

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,6 @@ module.exports = {
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-docs',
     '@storybook/addon-a11y',
     'storybook-css-modules-preset',
   ],
