0f44893923990b225416b40f90b274effc170284
keitakn <keita.koga.work@gmail.com>
2021-11-12 00:08:46 +0900

    :recycle: #140 GoogleTagManager用のComponentを作成し、それを利用するように変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,17 @@ import '../../styles/globals.css';
 import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import * as gtag from '../utils/gtag';
-import GoogleAnalytics from '../components/GoogleAnalytics';
+import { pageview, googleTagManagerId } from '../utils/gtm';
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from '../components/GoogleTagManager';
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
+      pageview(url);
     };
     router.events.on('routeChangeComplete', handleRouteChange);
 
@@ -21,7 +23,9 @@ const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
 
   return (
     <>
-      <GoogleAnalytics />
+      <GoogleTagManager
+        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+      />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </>
