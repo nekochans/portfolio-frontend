e9c6951cefbf173620c0514d6d64e46c9e486ded
keitakn <keita.koga.work@gmail.com>
2022-02-09 23:51:24 +0900

    :recycle: #142 Hero画像をstyled-componentsで表示させるところまで実装

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import mixins from '../styles/mixins';
 
 import ButtonGitHub from './ButtonGitHub';
+import hero from './hero.webp';
+
+const StyledSection = styled.section`
+  height: 650px;
+  background-image: url(${hero.src});
+  background-position: center;
+  background-size: cover;
+`;
+
+const StyledDiv = styled.div`
+  ${mixins.wContainer};
+  display: grid;
+  align-content: center;
+  justify-items: center;
+  height: 100%;
+`;
+
+// TODO h1 と pタグが崩れているので確認
 
 const Hero: React.VFC = () => (
-  <section className="hero">
-    <div className="hero-container w-container">
+  <StyledSection>
+    <StyledDiv>
       <h1>nekochans</h1>
       <p>We are a Freelance Web Engineer Organization.</p>
       <ButtonGitHub url="https://github.com/nekochans" />
-    </div>
-  </section>
+    </StyledDiv>
+  </StyledSection>
 );
 
 export default Hero;
