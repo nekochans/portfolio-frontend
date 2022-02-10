d2aac30abee9c23c35a2ab0298081eb374825e48
keitakn <keita.koga.work@gmail.com>
2022-02-10 22:53:39 +0900

    :wrench: #142 storybook-addon-nextを追加しwebpack5系で動作するように変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   stories: [
     '../src/components/**/*.stories.@(tsx|mdx)',
@@ -8,5 +10,18 @@ module.exports = {
     '@storybook/addon-a11y',
     '@storybook/addon-essentials',
     'storybook-css-modules-preset',
+    'storybook-addon-next',
   ],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    );
+
+    return config;
+  },
 };
