cabb54678f4cdbd0b25f9c0dab6eb3a4a54e897a
keitakn <keita.koga.work@gmail.com>
2023-01-23 12:31:28 +0900

    :art: #182 styled-components の設定が不要になったので CustomDocument をシンプルな形に変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,54 +1,15 @@
-import { Fragment } from 'react';
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default class CustomDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+const CustomDocument = (): JSX.Element => {
+  return (
+    <Html prefix="og: https://ogp.me/ns#">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: [
-          <Fragment key="1">
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </Fragment>,
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render(): JSX.Element {
-    return (
-      <Html prefix="og: https://ogp.me/ns#">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default CustomDocument;
